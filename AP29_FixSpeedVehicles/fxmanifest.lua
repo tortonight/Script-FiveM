953b6be3f8aaaa8cb3a98646a2e77deb11b9b992
LAM WAYUW <90068041+tortonight@users.noreply.github.com>
2022-04-16 19:18:28 +0800

    Add files via upload

---
+++ AP29_FixSpeedVehicles/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'AP29 .#2154'
+version '1.0.0'
+
+shared_script {
+    'config.lua',
+}
+
+client_script {
+    'src/client.lua',
+}
+
+server_script {
+    'src/server.lua',
+}
+
+dependencies {
+}
